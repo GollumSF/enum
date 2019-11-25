ef8319baf4b4ddfa09c8bbd00836b0815854a300
Damien Duboeuf <smeagolworms4@gmail.com>
2019-11-25 05:36:02 +0100

    Update EnumTrait.php

--- EnumTrait.php
+++ EnumTrait.php
@@ -3,14 +3,9 @@ namespace GollumSF\Enum;
 
 trait EnumTrait {
 	
-	private static $enums = null;
-	
 	public static function getEnums(): array {
-		if (self::$enums === null) {
-			$rClass = new \ReflectionClass(get_called_class());
-			self::$enums = $rClass->getConstants();
-		}
-		return self::$enums;
+		$rClass = new \ReflectionClass(get_called_class());
+		return $rClass->getConstants();
 	}
 	
 	public static function getKeys(): array {
